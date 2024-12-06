d1750915f5964bcc8d631217ec3e4abf2885da52
Luciano Pardo <pardobio@gmail.com>
2024-12-06 03:40:21 -0300

    css and userContext

--- src/components/Dasboard/Dashboard.jsx
+++ src/components/Dasboard/Dashboard.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Dashboard = ({ children }) => {
-    return (
-        <div className="dashboard-container">
-            {/* Header */}
-            <Header />
 
-            {/* Sidebar */}
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            <Header user={user} isAuthenticated={isAuthenticated} />
             <Sidebar />
 
             {/* Main Content Area */}
             <div className="dashboard-content">
-                <main>{children}</main>
+                <main>
+                    {React.cloneElement(children, { user, isAuthenticated })}
+                </main>
             </div>
-        </div>
+        </>
     );
 };
--- src/components/Dasboard/Header/Header.jsx
+++ src/components/Dasboard/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import walletIcon from '../../../assets/wallet-2.svg'
+import userIcon from '../../../assets/user-icon.svg'
+
 import './Header.css'
 
-export const Header = () => {
+export const Header = ({ user, isAuthenticated }) => {
   return (
     <header className="header">
       <div className="left-section"></div>
       <div className="right-section">
         <div id="wallet-icon" className="wallet">
           <div>
-            <img src="assets/wallet-2.svg" alt="Wallet" className="logo-img" />
+            <img src={walletIcon} alt="w" className="logo-img" />
           </div>
           {/* Pass visibility state and handle close event */}
           {/* <app-wallet-connect
@@ -16,10 +19,10 @@ export const Header = () => {
               (close)="closeWalletModal()"
             ></app-wallet-connect> */}
         </div>
-        <a href="/dashboard/profile" className="user">
-          <img src="assets/user-icon.svg" alt="User" className="logo-img" />
-          <span /* *ngIf="user$ | async as user" */ className="user-name">
-            {/* {{ user.name ? user.name : 'User Name' }} */} user name
+        <a href="/dashboard/home" className="user">
+          <img src={userIcon} alt="User" className="logo-img" />
+          <span className="user-name">
+            {isAuthenticated? user.name : 'Guest'}
           </span>
         </a>
       </div >
